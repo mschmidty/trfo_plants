3d6ad7732f5673cdaca6ce436bf98d7dcad3280d
Mike Schmidt <mschmidty@gmail.com>
2020-01-12 16:59:53 -0700

    fixed design of detail

--- trfo_plants/urls.py
+++ trfo_plants/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name = 'index.html'), name = "home"),
     path('plants/', include('plants.urls')),
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name="register"),
+    #path('create-new-user/', user_views.register, name="register"),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name="logout"),
 ]
